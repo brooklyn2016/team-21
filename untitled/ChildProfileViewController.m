1651dde7c6af331c7a87e7ccffdaaedaab08efa6
Benjamin Yi <bentaebox@gmail.com>
2016-10-29 05:19:59 -0400

    Create child profile view controller

---
+++ untitled/ChildProfileViewController.m
@@ -0,0 +1,13 @@
+//
+//  ChildProfileViewController.m
+//  untitled
+//
+//  Created by Benjamin Yi on 10/29/16.
+//  Copyright © 2016 team21. All rights reserved.
+//
+
+#import "ChildProfileViewController.h"
+
+@implementation ChildProfileViewController
+
+@end
