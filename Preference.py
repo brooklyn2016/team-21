74f8a9239899932a9bfff9bd82e3e654b10ebb8b
tgb34 <timothy.birkner@rutgers.edu>
2016-10-29 03:10:05 -0400

    Reinforce Autistic Users with Positive Feedback Pictures

---
+++ Preference.py
@@ -0,0 +1,24 @@
+import random
+print random.random()
+
+preference = Reinforcement.Reinforcement(choice)
+
+if(preference == "cars"):
+	disp(cars[randrange(0, 3.1)])
+
+elif(preference == "science"):
+	disp(science[randrange(0,3.1)])
+
+elif(preference == "games"):
+	disp(games[randrange(0,3.1)])
+
+elif(preference == "animals"):
+	disp(animals[randrange(0,3.1)])
+
+"""Splash Screen with 'Good Job!' or something on top of image?"""
+
+if(delete):
+	preference.delete(array[])
+
+if(add):
+	preference.add(array[],"string")
