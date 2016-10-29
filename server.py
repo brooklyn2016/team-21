e2154bad4a6bcc6688e75c8863f2d36cd570f2e1
SupaStuff <StuffQilla@gmail.com>
2016-10-29 03:19:59 -0400

    removed 2 children

--- server.py
+++ server.py
@@ -12,11 +12,15 @@ roster = [
         {'name': "Molly", 'img': "https://sumonyusuf.files.wordpress.com/2014/09/03_mg_0309.jpg"},
         {'name': "Carlos", 'img': "http://i.usatoday.net/news/_photos/2012/05/14/1-in-3-autistic-young-adults-lack-jobs-education-DU1FRCH6-x-large.jpg"},
         {'name': "Pete", 'img': "http://media3.s-nbcnews.com/i/newscms/2016_29/1143945/ralph-autism-pokemon-drawing3-today-tease-160718_d99bb087a083df8258a0a36d16246bfc.jpg"},
-        {'name': "Joshua", 'img': "http://cdn.images.express.co.uk/img/dynamic/1/590x/autistic-378222.jpg"},
+        #{'name': "Joshua", 'img': "http://cdn.images.express.co.uk/img/dynamic/1/590x/autistic-378222.jpg"},
         {'name': "Ida", 'img': "http://www.missidahousa.com/contestants/hires/Ariana%20Whitfield%20-%20Miss%20Coeur%20dAlene%20Teen%20USA%202013lo.jpg"},
-        {'name': "Scott", 'img': "https://www.libertymutual.com/insurance-content/title-hero/auto/car-insurance-for-teens/auto-insurance-for-teenagers.jpg?v=2015101808324220"},
+        #{'name': "Scott", 'img': "https://www.libertymutual.com/insurance-content/title-hero/auto/car-insurance-for-teens/auto-insurance-for-teenagers.jpg?v=2015101808324220"},
         {'name': "Panda", 'img': "http://www.hiphopfind.com/upload/ciggjljpms.jpg"},
         ]
+
+def send_json(l):
+    return jsonify(**{'data':l}) #this was ugly. Needed to move it 
+
 
 @app.route('/')
 def hello_world():
@@ -24,7 +28,7 @@ def hello_world():
 
 @app.route('/getRoster', methods=['GET'])
 def getRoster():
-    return jsonify(**{'data': roster})#json.dumps(roster)
+    return send_json(roster)#json.dumps(roster)
 
 @app.route('/meaning', methods=['GET', 'POST'])
 def meaning():
