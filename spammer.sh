9b75e8021760b636f0a185d705887558ed999857
SupaStuff <StuffQilla@gmail.com>
2016-10-29 07:41:08 -0400

    simplified spammer.sh

--- spammer.sh
+++ spammer.sh
@@ -1,19 +1,10 @@
-#!/bin/expect
-
-read -s -p "Enter Username: " username
-read -s -p "Enter Password(so insecure): " password
-
+#!/bin/bash
 
 for i in {1..1000};
 do 
 	echo new commit >> spam
 	git commit -am "Destroyable History $i"
-	spawn "git push"
-	expect "Username"
-	send "$username\r"
-	expect "Password"
-	send "$password\r"
-	interact
+	git push
 	sleep 3s
 done
 
