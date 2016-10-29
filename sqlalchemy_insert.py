fde3f41c26359feac00a72592623d62d7b12f9b4
Moulindra Muchumari <moulindra1729@gmail.com>
2016-10-29 03:22:02 -0400

    gitignore edited for db file

--- sqlalchemy_insert.py
+++ sqlalchemy_insert.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy_declarative import Child, SoundRecordings
+from struct import pack
+from sqlalchemy_declarative import Child, SoundRecordings, Base
 
 engine = create_engine('sqlite:///autistic.db')
 
@@ -12,38 +12,38 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 new_child = Child(name='Ann')
-session.add(new_person)
+session.add(new_child)
 session.commit()
 
 new_child = Child(name='John')
-session.add(new_person)
+session.add(new_child)
 session.commit()
 
 new_child = Child(name='Mary')
-session.add(new_person)
+session.add(new_child)
 session.commit()
 
 new_child = Child(name='James')
-session.add(new_person)
+session.add(new_child)
 session.commit()
 
 new_child = Child(name='Harry')
-session.add(new_person)
+session.add(new_child)
 session.commit()
 
-new_sndRcrd = SoundRecordings(child_id=1, recording=pack('A', 365))
+new_sndRcrd = SoundRecordings(child_id=1, recording=pack('H', 365))
 session.add(new_sndRcrd)
 
-new_sndRcrd = SoundRecordings(child_id=2, recording=pack('B', 365))
+new_sndRcrd = SoundRecordings(child_id=2, recording=pack('H', 210))
 session.add(new_sndRcrd)
 
-new_sndRcrd = SoundRecordings(child_id=1, recording=pack('C', 365))
+new_sndRcrd = SoundRecordings(child_id=1, recording=pack('H', 212))
 session.add(new_sndRcrd)
 
-new_sndRcrd = SoundRecordings(child_id=3, recording=pack('D', 365))
+new_sndRcrd = SoundRecordings(child_id=3, recording=pack('H', 100))
 session.add(new_sndRcrd)
 
-new_sndRcrd = SoundRecordings(child_id=2, recording=pack('E', 365))
+new_sndRcrd = SoundRecordings(child_id=2, recording=pack('H', 89))
 session.add(new_sndRcrd)
 
 session.commit()
