4c3b357b97e446dc51cdac25fe81c858b9600179
Benjamin Yi <bentaebox@gmail.com>
2016-10-29 04:02:17 -0400

    Add viewController to create caretaker profiler

---
+++ untitled/CaretakerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaretakerViewController.h
+//  untitled
+//
+//  Created by Benjamin Yi on 10/29/16.
+//  Copyright © 2016 team21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaretakerViewController : UIViewController
+
+@end
---
+++ untitled/CaretakerViewController.m
@@ -0,0 +1,13 @@
+//
+//  CaretakerViewController.m
+//  untitled
+//
+//  Created by Benjamin Yi on 10/29/16.
+//  Copyright © 2016 team21. All rights reserved.
+//
+
+#import "CaretakerViewController.h"
+
+@implementation CaretakerViewController
+
+@end
