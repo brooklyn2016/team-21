854b926793f1ed22f2243a061615a582e5f822e0
SupaStuff <StuffQilla@gmail.com>
2016-10-28 22:15:15 -0400

    added endpoints

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return ':p'
+
+@app.route('/getRoster', methods=['GET'])
+def getRoster():
+    return json.dumps(["John", "Joe", "Jill", "Jane"])
+
+@app.route('/meaning', methods=['GET', 'POST'])
+def meaning():
+    if request.method == 'POST':
+        return "This means: Apple"
+    else:
+        return "Meaning uploaded!! :D"
 
 if __name__ == "__main__":
     if os.environ.get('FORCE_B_W_U') == 'None':
